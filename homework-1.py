a45718624fcbf21be3624dd01569bb5064b3f325
saichandrapranav <100420102+saichandrapranav@users.noreply.github.com>
2022-08-16 18:04:53 +0530

    Add files via upload

---
+++ homework-1.py
@@ -0,0 +1,10 @@
+#print = ast,sr,gita
+
+String = 'ASTRING'
+#         0123456
+#  G=-1,N=-2,I=-3,R=-4,T=-5,S=-6,A=-7
+#  A=-7,S=-6,T=-5,R=-4,I=-3,N=-2,G=-1
+
+print(String[:3])
+print(String[1:5:2])
+print(String[-1:-12:-2])
